76f84c64aa2ab8c35f41922ec720426ed149344a
Pranith Mahanti <pranithmahanti@gmail.com>
2026-06-12 12:32:15 +0530

    Basic Func #01

---
+++ main.py
@@ -0,0 +1,15 @@
+from asyncio import all_tasks
+import os 
+from os.path import isdir
+import pathlib
+
+def getAllItems(mainPath):
+    return list(pathlib.Path(mainPath).rglob("*"))
+
+if __name__ == "__main__":
+    MUSIC_DIR = os.path.expanduser("~/Music/")
+    allItems = getAllItems(MUSIC_DIR)
+
+    for item in allItems:
+        if not os.path.isdir(item):
+            print(item)
